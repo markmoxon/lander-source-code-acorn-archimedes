f66b75067ab1d75d9b45bc8ca589ec998cee7b1c
Mark Moxon <mark@moxon.net>
2024-02-10 00:27:49 +0000

    Remove encryption and Absolute loader from main source into a separate RISC OS source file

--- 2-build-files/convert-to-vasm.py
+++ 2-build-files/convert-to-vasm.py
@@ -14,68 +14,81 @@
 import re
 
 
+def convert(input_file, output_file):
+
+    for line in input_file:
+
+        # Strip comments
+        line = re.sub(r" *[;\\].*$", "", line)
+
+        # .label -> label:
+        line = re.sub(r"^\.([^ \n]+)", r"\1:", line)
+
+        # x = y -> .set x, y
+        line = re.sub(r"^ ?(.+) = (.+)$", r".set \1, \2", line)
+
+        # ORG -> .org
+        line = re.sub(r"^ ?ORG ", ".org ", line)
+
+        # SKIP -> .skip
+        line = re.sub(r"^ ?SKIP ", ".skip ", line)
+
+        # P% -> $
+        line = re.sub(r"P%", "$", line)
+
+        # Binary % -> 0x
+        binary_string = re.search(r"%([0-1]+)", line)
+        if binary_string:
+            hex_string = hex(int(binary_string.group(1), 2))
+            line = re.sub(r"%([0-1]+)", hex_string, line)
+
+        # Hexadecimal & -> 0x
+        line = re.sub(r"&", "0x", line)
+
+        # EQUD -> .long
+        line = re.sub(r"^ ?EQUD ", ".long ", line)
+
+        # EQUW -> .word
+        line = re.sub(r"^ ?EQUW ", ".word ", line)
+
+        # EQUS -> .byte
+        line = re.sub(r"^ ?EQUS ", ".byte ", line)
+
+        # EQUB -> .byte
+        line = re.sub(r"^ ?EQUB ", ".byte ", line)
+
+        # ALIGN -> .balign
+        line = re.sub(r"^ ?ALIGN", ".balign 4", line)
+
+        # IF -> .ifdef
+        line = re.sub(r"^ ?IF ", ".ifdef ", line)
+
+        # ENDIF -> .endif
+        line = re.sub(r"^ ?ENDIF", ".endif", line)
+
+        # ELSE -> .else
+        line = re.sub(r"^ ?ELSE", ".else", line)
+
+        # INCBIN -> .incbin
+        line = re.sub(r"^ ?INCBIN", ".incbin", line)
+
+        # Write updated line
+        if line.strip():
+            output_file.write(line)
+
+
 print("Converting 1-source-files/Lander.arm")
 
 bbc_basic_file = open("1-source-files/main-sources/Lander.arm", "r")
 vasm_file = open("3-assembled-output/Lander.arm", "w")
-
-for line in bbc_basic_file:
-
-    # Strip comments
-    line = re.sub(r" *[;\\].*$", "", line)
-
-    # .label -> label:
-    line = re.sub(r"^\.([^ \n]+)", r"\1:", line)
-
-    # x = y -> .set x, y
-    line = re.sub(r"^ ?(.+) = (.+)$", r".set \1, \2", line)
-
-    # ORG -> .org
-    line = re.sub(r"^ ?ORG ", ".org ", line)
-
-    # SKIP -> .skip
-    line = re.sub(r"^ ?SKIP ", ".skip ", line)
-
-    # P% -> $
-    line = re.sub(r"P%", "$", line)
-
-    # Binary % -> 0x
-    binary_string = re.search(r"%([0-1]+)", line)
-    if binary_string:
-        hex_string = hex(int(binary_string.group(1), 2))
-        line = re.sub(r"%([0-1]+)", hex_string, line)
-
-    # Hexadecimal & -> 0x
-    line = re.sub(r"&", "0x", line)
-
-    # EQUD -> .long
-    line = re.sub(r"^ ?EQUD ", ".long ", line)
-
-    # EQUW -> .word
-    line = re.sub(r"^ ?EQUW ", ".word ", line)
-
-    # EQUS -> .byte
-    line = re.sub(r"^ ?EQUS ", ".byte ", line)
-
-    # EQUB -> .byte
-    line = re.sub(r"^ ?EQUB ", ".byte ", line)
-
-    # ALIGN -> .balign
-    line = re.sub(r"^ ?ALIGN", ".balign 4", line)
-
-    # IF -> .ifdef
-    line = re.sub(r"^ ?IF ", ".ifdef ", line)
-
-    # ENDIF -> .endif
-    line = re.sub(r"^ ?ENDIF", ".endif", line)
-
-    # ELSE -> .else
-    line = re.sub(r"^ ?ELSE", ".else", line)
-
-    # Write updated line
-    if line.strip():
-        vasm_file.write(line)
-
+convert(bbc_basic_file, vasm_file)
 bbc_basic_file.close()
+vasm_file.close()
+
+bbc_basic_file = open("1-source-files/main-sources/RunImage.arm", "r")
+vasm_file = open("3-assembled-output/RunImage.arm", "w")
+convert(bbc_basic_file, vasm_file)
+bbc_basic_file.close()
+vasm_file.close()
 
 print("3-assembled-output/Lander.arm file saved")
--- Makefile
+++ Makefile
@@ -20,11 +20,11 @@ all:
 	cp 1-source-files/other-sources/arthur/Lander,ffb 5-compiled-game-discs/arthur/Lander,ffb
 	cp 3-assembled-output/GameCode.bin 5-compiled-game-discs/arthur/GameCode
 
-	$(VASM) -a2 -m2 -quiet -Fbin -D_RISCOS=1 -L 3-assembled-output/compile.txt -o 3-assembled-output/!RunImage.bin 3-assembled-output/Lander.arm
+	$(VASM) -a2 -m2 -quiet -Fbin -L 3-assembled-output/compile2.txt -o 3-assembled-output/!RunImage.unprot.bin 3-assembled-output/RunImage.arm
 	cp 1-source-files/other-sources/riscos/!Run,feb 5-compiled-game-discs/riscos/!Lander/!Run,feb
 	cp 1-source-files/other-sources/riscos/!Sprites,ff9 5-compiled-game-discs/riscos/!Lander/!Sprites,ff9
 	cp 1-source-files/other-sources/riscos/MemAlloc,ffa 5-compiled-game-discs/riscos/!Lander/MemAlloc,ffa
-	cp 3-assembled-output/!RunImage.bin 5-compiled-game-discs/riscos/!Lander/!RunImage,ff8
+	cp 3-assembled-output/!RunImage.unprot.bin 5-compiled-game-discs/riscos/!Lander/!RunImage,ff8
 
 	@$(PYTHON) 2-build-files/crc32.py 4-reference-binaries 3-assembled-output
 
